ff88870c6829d101a565895e199285480a62e152
Gaurav Sharma <sharmag8121@gmail.com>
2025-02-13 04:55:00 +0530

    Nested Routes and Index Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
-import Product from './pages/Product'
-import Homepage from './pages/Homepage'
-import Pricing from './pages/Pricing'
-import AppLayout from './pages/AppLayout'
-import Login from './pages/Login'
-import PageNotFound from './pages/PageNotFound'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 
 export default function App() {
-  
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage/>} />
-        <Route path="product" element={<Product/>} />
-        <Route path="pricing" element={<Pricing/>} />
-        <Route path="login" element={<Login/>} />
-        <Route path="app" element={<AppLayout/>} />
-        <Route path="*" element={<PageNotFound/>} />
+        <Route index element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of cities</p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
